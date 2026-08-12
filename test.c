c31dea5998f3266bc0c330ad47c86aad6c038625
ViniZ <viniciusizidrop@gmail.com>
2026-08-12 21:39:48 +0000

    Incluindo o codigo da aula1

---
+++ test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int nota1;
+    int nota2;
+    int media;
+    nota1 = 6;
+    nota2 = 9;
+    media = (nota1 + nota2) / 2;
+    printf("Media = %d", media);
+    return 0;  
+}
