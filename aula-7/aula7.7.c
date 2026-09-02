3d5dd62efd02aa110c464e958cd2439b5fa2a538
ViniZ <viniciusizidrop@gmail.com>
2026-09-02 23:51:22 +0000

    terminando o codigo 7.7

--- aula-7/aula7.7.c
+++ aula-7/aula7.7.c
@@ -3,22 +3,29 @@
 //calcule e imprima a média, a maior e a menor nota.
 #include <stdio.h>
 int main(){
-    float notas,nota;
-    float media,menor,maior,soma;
+    float nota;
+    float media,menor,maior,soma=0;
     for(int i=1; i<=10;i++){
         printf("Digite uma nota: ");
-        scanf("%f",&notas);
-
-        soma += notas;
-        if(notas < nota){
-            menor = notas;
-        }
-        if(notas > nota){
-            maior = notas;
-        }
-        media = soma / 10;
-        nota = notas;
+        scanf("%f",&nota);
+        if(nota<=10 && nota >=0) {
+            soma += nota;
+            if (i==1){
+                maior = nota;
+                menor = nota;
+            }
+            else{
+                if(nota>maior){maior=nota;}
+                if(nota<menor){menor=nota;}
+            }
+    
     }
-    printf("A menor nota é: %f, A maior é %f, é a Media é %f ",menor,maior,media);
+else{
+    printf("Nota invalida\n");
+    return 0;
+}
+    }
+    media = soma / 10;
+    printf("A menor nota é: %.2f, A maior é %.2f, é a Media é %.2f\n ",menor,maior,media);
 
 }
