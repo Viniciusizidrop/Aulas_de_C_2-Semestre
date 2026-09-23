0a507f389be4eca6a9deff12856bc966f4979e6c
Viniciusizidrop <viniciusizidrop@gmail.com>
2026-09-23 10:59:33 -0300

    aula12 biblioteca de cpf

---
+++ aula-12/aula12.1.c
@@ -0,0 +1,7 @@
+#include "aula12.2.h"
+//gcc aula12.1.c aula12.2.c -o libcpf && .\libcpf
+int main(){
+    //executa_cpf();
+    gera_cpf();
+    return 0;
+}
---
+++ aula-12/aula12.2.h
@@ -0,0 +1,7 @@
+#ifndef LIBCPF
+#define LIBCPF
+
+void executa_cpf();
+void gera_cpf();
+
+#endif
