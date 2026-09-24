f6b6b9a9a3e81a0aceeb1eba88bd894754c6baef
Viniciusizidrop <viniciusizidrop@gmail.com>
2026-09-24 10:52:58 -0300

    "aula13"

--- aula-12/aula12.2.c
+++ aula-12/aula12.2.c
@@ -32,10 +32,34 @@
 
 }
 
+int calc_dv(int soma){
+       int resto = soma % 11;
+       int dv;
+    if (resto < 2) {dv = 0;} 
+    else {dv = 11 - resto;}
+    return dv;
+}
+
 void gera_cpf(){
     srand(time(NULL));
+    int n,soma1=0,soma2=0,j=10,dv1=0,dv2=0;
     for(int i=1; i<=9; i++){
         int n = rand() % 10;
         printf("O numero%d gerado eh: %d\n",i,n);
+  
+    soma1 += (n*j);
+    if(i>=2){
+        if(i!=9){
+            soma2+=(n*(j+1));}
+        else{
+            dv1=calc_dv(soma1);
+            soma2 += (dv1*(j+1));
+            dv2=calc_dv(soma2);}
     }
+    j--;   
+}
+printf("O dv1: %d\n",dv1);
+printf("O dv2: %d\n",dv2);
+
+
 }
